d5049fe019fca09e7a69657fe983a1cbe7cf462a
Maxim <maxim.oweyssi@gmail.com>
2024-10-29 17:41:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -374,7 +374,7 @@ fig.update_layout(
 )
 
 col1.plotly_chart(fig)
-mcol1, mcol2, mcol3 = st.columns(3)
+mcol1, mcol2, mcol3 = col1.columns(3)
 if HS_select == []:
     mcol1.metric("Vybraný český export za rok 2022", "{:,.0f}".format(sum(filtered_df['CZ Export 2022 CZK'])/1000000000),'miliard CZK' )
     mcol2.metric("Vybraný český export 2025 až 2030", "{:,.0f}".format(sum(filtered_df['CZ Celkový Export 25-30 CZK'])/1000000000), "miliard CZK")
