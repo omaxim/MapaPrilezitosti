db787e230a0256cd48431e542ff7d11836bc8a73
Maxim <maxim.oweyssi@gmail.com>
2024-10-30 12:37:24 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -390,8 +390,7 @@ vega_chart_spec = {
         "contains": "padding",
         "resize": True  # Allow resizing
     },
-    "height": 800,  # Fixed height to ensure taller display (adjust as needed)
-    "width": {"step": 150}  # Dynamic width based on the number of marks or data points
+    "height": 800  # Fixed height to ensure taller display (adjust as needed)
 }
 # Display the Vega-Lite chart in Streamlit
 col1.vega_lite_chart(selected_data, vega_chart_spec, use_container_width=True)
