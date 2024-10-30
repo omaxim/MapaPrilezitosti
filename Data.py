f52377e457e2dac6fa628d4f4025418a597745b2
Maxim <maxim.oweyssi@gmail.com>
2024-10-30 11:06:47 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -367,7 +367,6 @@ vega_chart_spec = {
 }
 
 # Display the Vega-Lite chart in Streamlit
-col1 = st.container()
 col1.vega_lite_chart(selected_data, vega_chart_spec, use_container_width=True)
 
 # Display metrics in columns
