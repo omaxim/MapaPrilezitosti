84f00c124cc7c348d16761fe83e40315fd9554f9
Maxim <maxim.oweyssi@gmail.com>
2024-10-30 12:35:23 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -390,10 +390,10 @@ vega_chart_spec = {
         "contains": "padding",
         "resize": True  # Optional: allow resizing if needed
     },
-    "height": {"step": 2550}  # Set a minimum height for each mark, adjust the step as needed
+    "height": {"step": 33550}  # Set a minimum height for each mark, adjust the step as needed
 }
 # Display the Vega-Lite chart in Streamlit
-col1.vega_lite_chart(selected_data, vega_chart_spec)
+col1.vega_lite_chart(selected_data, vega_chart_spec, use_container_width=True)
 
 # Display metrics in columns
 mcol1, mcol2, mcol3 = col1.columns(3)
