531311f39ea70b6701a57caff93cdc62419952fc
Maxim <maxim.oweyssi@gmail.com>
2024-11-04 23:57:54 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -29,7 +29,7 @@ st.markdown("""
                     padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
-                    color: black
+                    color: "black"
                 }
         </style>
         """, unsafe_allow_html=True)
