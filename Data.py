34199a930862b432a91fd9617fde5d6c52340c67
Maxim <maxim.oweyssi@gmail.com>
2024-11-04 22:39:55 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -10,6 +10,29 @@ st.set_page_config(
     page_icon="favicon.ico",
     layout="wide"
 )
+pages = ["Home", "Library", "Tutorials", "Development", "Download"]
+styles = {
+    "nav": {
+        "background-color": "#7BD192",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "border-radius": "0.5rem",
+        "padding": "0.4375rem 0.625rem",
+        "margin": "0 0.125rem",
+    },
+    "active": {
+        "background-color": "rgba(255, 255, 255, 0.25)",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
+
+page = st_navbar(pages, styles=styles)
+st.write(page)
 st.markdown("""
 <style>
     [data-testid=stSidebar] {
