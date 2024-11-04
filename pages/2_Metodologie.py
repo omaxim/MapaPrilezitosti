864788aace88d228fad0f48ee759ae3c1d66af22
Maxim <maxim.oweyssi@gmail.com>
2024-11-04 18:11:35 +0000

    Update 2_Metodologie.py

--- pages/2_Metodologie.py
+++ pages/2_Metodologie.py
@@ -5,6 +5,6 @@ st.logo('logo.svg')
 """
 # Metodologie
 
-
+Tato aplikace se opírá o harmonizovaný dataset [BACI](https://www.cepii.fr/CEPII/en/bdd_modele/bdd_modele_item.asp?id=37), který je každý rok publikován organizací CEPII
 
 """
