fff1712aa477de33b180a19823c18a36ad64494e
Maxim <maxim.oweyssi@gmail.com>
2024-11-04 18:13:36 +0000

    Update 2_Metodologie.py

--- pages/2_Metodologie.py
+++ pages/2_Metodologie.py
@@ -5,6 +5,9 @@ st.logo('logo.svg')
 """
 # Metodologie
 
-Tato aplikace se opírá o harmonizovaný dataset [BACI](https://www.cepii.fr/CEPII/en/bdd_modele/bdd_modele_item.asp?id=37), který je každý rok publikován organizací CEPII
+Tato aplikace se opírá o harmonizovaný dataset [BACI](https://www.cepii.fr/CEPII/en/bdd_modele/bdd_modele_item.asp?id=37), který je každý rok publikován organizací CEPII.
+CEPII pomocí své publikované metodologie harmonizuje data z UN databáze Comtrade tah, aby se každý export rovnal importu a předešlo se dvojímu počítání obchodních toků.
+
+
 
 """
