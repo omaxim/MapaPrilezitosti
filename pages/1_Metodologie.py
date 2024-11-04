f42b54501904767cfe48ba6ff3992a970bd5b96c
Maxim <maxim.oweyssi@gmail.com>
2024-11-04 23:41:10 +0000

    Update 1_Metodologie.py

--- pages/1_Metodologie.py
+++ pages/1_Metodologie.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+st.set_page_config(
+    page_title="Mapa Příležitostí",
+    page_icon="favicon.png",
+    layout="wide"
+)
 st.markdown("""
 <style>
     /* Set sidebar background color */
