927fd208ab5e42d7df06e5082ce515c10ab81154
Maxim <maxim.oweyssi@gmail.com>
2024-11-05 10:57:05 +0000

    Update visualsetup.py

--- visualsetup.py
+++ visualsetup.py
@@ -95,13 +95,13 @@ def load_visual_identity(header_image_path):
         
     </style>
     """, unsafe_allow_html=True)
-    #st.markdown("""
-    #        <style>
-    #               .block-container {
-    #                    padding-top: 1rem;
-    #                    padding-bottom: 5rem;
-    #                    padding-left: 5rem;
-    #                    padding-right: 5rem;
-    #                }
-    #        </style>
-    #        """, unsafe_allow_html=True)
+    st.markdown("""
+            <style>
+                   .block-container {
+                        padding-top: 1rem;
+                        padding-bottom: 5rem;
+                        padding-left: 5rem;
+                        padding-right: 5rem;
+                    }
+            </style>
+            """, unsafe_allow_html=True)
