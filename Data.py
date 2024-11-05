27e611cdb441d815c6e10070c7528728547b8cd8
Maxim <maxim.oweyssi@gmail.com>
2024-11-05 10:18:17 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -41,8 +41,8 @@ st.markdown(
             position: absolute;
             bottom: 0;
             width: 100%;
-            height: 100%;  /* Match header-image height */
-            background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 1) 100%);
+            height: 50%;  /* Match header-image height */
+            background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 60%, rgba(255, 255, 255, 1) 100%);
             z-index: 1;
         }}
 
