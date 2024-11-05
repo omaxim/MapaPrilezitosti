0a7db619cd0588c3599b987eb76de07e9c3c0a45
Maxim <maxim.oweyssi@gmail.com>
2024-11-05 09:40:13 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -21,16 +21,16 @@ st.markdown("""
     }
 </style>
 """, unsafe_allow_html=True)
-st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 5rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
+#st.markdown("""
+#        <style>
+#               .block-container {
+#                    padding-top: 1rem;
+#                    padding-bottom: 5rem;
+#                    padding-left: 5rem;
+#                    padding-right: 5rem;
+#                }
+#        </style>
+#        """, unsafe_allow_html=True)
 
 # Load the image and encode it in Base64 format
 with open("header.jpg", "rb") as image_file:
