3f1b34344c0f058a16cec92d26cc82f13e4c07ae
Maxim <maxim.oweyssi@gmail.com>
2024-11-05 09:18:59 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -386,7 +386,7 @@ else:
 fig.update_layout(
     hoverlabel=dict(
         font_family="verdana",
-        bgcolor=color_discrete_map[color],
+        bgcolor=color_discrete_map[filtered_df[color]],
         font_color="#FFFFFF"
     ),
     legend=dict(
