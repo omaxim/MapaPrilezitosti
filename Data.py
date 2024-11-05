b143ed7cfaea65ff67f5e73004deb979185b6b82
Maxim <maxim.oweyssi@gmail.com>
2024-11-05 00:02:04 +0000

    Update Data.py

--- Data.py
+++ Data.py
@@ -25,7 +25,7 @@ st.markdown("""
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 0rem;
+                    padding-bottom: 1rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
                 }
