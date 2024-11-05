6cfa3938bb381578873fcc672fa7b29e40178d92
Maxim <maxim.oweyssi@gmail.com>
2024-11-05 10:41:46 +0000

    Update visualsetup.py

--- visualsetup.py
+++ visualsetup.py
@@ -2,9 +2,7 @@ import streamlit as st
 import base64
 
 def load_visual_identity(header_image_path):
-    # Load the image and encode it in Base64 format
-    with open(header_image_path, "rb") as image_file:
-        header_image = base64.b64encode(image_file.read()).decode("utf-8")
+
     # Load Montserrat font from Google Fonts
     st.markdown(
         """
@@ -24,69 +22,104 @@ def load_visual_identity(header_image_path):
         """,
         unsafe_allow_html=True
     )
+
+
+    # Load the image and encode it in Base64 format
+    with open(header_image_path, "rb") as image_file:
+        header_image = base64.b64encode(image_file.read()).decode("utf-8")
+        
+    #st.markdown(
+    #    f"""
+    #    <style>
+    #        /* Remove default padding and margin from body and html to prevent gaps */
+    #        html, body {{
+    #            margin: 0;
+    #            padding: 0;
+    #        }}
+#
+    #        /* Make header fixed at the top with full viewport width */
+#    #        .header-image {{
+#    #            position: fixed;
+#    #            top: 0;
+#    #            left: 0;
+#    #            width: 100vw;
+#    #            height: 200px;  /* Adjust height as needed */
+#    #            background-image: url("data:image/jpg;base64,{header_image}");
+#    #            background-size: cover;
+#    #            background-position: center;
+#    #            background-repeat: no-repeat;
+#    #            z-index: 0;
+#    #        }}
+#
+#    #        /* Extend the gradient to blend into content smoothly */
+#    #        .header-gradient {{
+#    #            position: absolute;
+#    #            bottom: 0;
+#    #            width: 100%;
+#    #            height: 80%;  /* Match header-image height */
+#    #            background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 1) 100%);
+#    #            z-index: 1;
+#    #        }}
+#
+    #        /* Create overlap effect with main content */
+    #        .main-content {{
+    #            margin-top: -150px;  /* Negative margin to pull content up */
+    #            z-index: 0;         /* Ensures it sits below the header */
+    #            position: relative;
+    #        }}
+    #    </style>
+    #    <div class="header-image">
+    #        <div class="header-gradient"></div>
+    #    </div>
+    #    """,
+    #    unsafe_allow_html=True
+    #)
+
     st.markdown(
-        f"""
+        """
         <style>
-            /* Remove default padding and margin from body and html to prevent gaps */
-            html, body {{
-                margin: 0;
-                padding: 0;
-            }}
+        [data-testid="stAppViewContainer"] > .main {{
+        background-image: url("data:image/png;base64,{header_image}");
+        background-size: 180%;
+        background-position: top left;
+        background-repeat: no-repeat;
+        background-attachment: local;
+        }}
 
-            /* Make header fixed at the top with full viewport width */
-            .header-image {{
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100vw;
-                height: 200px;  /* Adjust height as needed */
-                background-image: url("data:image/jpg;base64,{header_image}");
-                background-size: cover;
-                background-position: center;
-                background-repeat: no-repeat;
-                z-index: 0;
-            }}
+        [data-testid="stSidebar"] > div:first-child {{
+        background-image: url("data:image/png;base64,{header_image}");
+        background-position: center; 
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        }}
 
-            /* Extend the gradient to blend into content smoothly */
-            .header-gradient {{
-                position: absolute;
-                bottom: 0;
-                width: 100%;
-                height: 80%;  /* Match header-image height */
-                background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 1) 100%);
-                z-index: 1;
-            }}
+        [data-testid="stHeader"] {{
+        background: rgba(0,0,0,0);
+        }}
 
-            /* Create overlap effect with main content */
-            .main-content {{
-                margin-top: -150px;  /* Negative margin to pull content up */
-                z-index: 0;         /* Ensures it sits below the header */
-                position: relative;
-            }}
+        [data-testid="stToolbar"] {{
+        right: 2rem;
+        }}
         </style>
-        <div class="header-image">
-            <div class="header-gradient"></div>
-        </div>
-        """,
-        unsafe_allow_html=True
+        """
     )
 
-    st.markdown("""
-    <style>
-        /* Set sidebar background color */
-        [data-testid="stSidebar"] {
-            background-color: #000000;
-        }
-
-        /* Set sidebar text color */
-        [data-testid="stSidebar"] * {
-            color: #FFFFFF;
-        }
-        [data-testid="stHeader"] {
-            background-color: rgba(0,0,0,0);
-        }
-    </style>
-    """, unsafe_allow_html=True)
+    #st.markdown("""
+    #<style>
+    #    /* Set sidebar background color */
+    #    [data-testid="stSidebar"] {
+    #        background-color: #000000;
+    #    }
+#
+    #    /* Set sidebar text color */
+    #    [data-testid="stSidebar"] * {
+    #        color: #FFFFFF;
+    #    }
+    #    [data-testid="stHeader"] {
+    #        background-color: rgba(0,0,0,0);
+    #    }
+    #</style>
+    #""", unsafe_allow_html=True)
     st.markdown("""
             <style>
                    .block-container {
