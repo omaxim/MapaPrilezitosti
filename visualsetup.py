7e1c76a768e39c30dbbb8f16626524d0e717e791
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 12:14:21 +0000

    Update visualsetup.py

--- visualsetup.py
+++ visualsetup.py
@@ -45,7 +45,7 @@ def load_visual_identity(header_image_path):
                 top: -5vw;
                 left: -10vw;
                 overflow-x: hidden;
-                width: 120%;
+                width: 119%;
                 box-sizing: border-box;
                 height: 30vw;  /* Adjust height as needed */
                 background-image: url("data:image/jpg;base64,{header_image}");
