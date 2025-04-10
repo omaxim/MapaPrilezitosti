ecd58293b6d4230b9a196dcf106d63b06e275662
omaxim <maxim.oweyssi@gmail.com>
2025-04-10 21:20:08 +0200

    Update chartjsbubble.py

--- chartjsbubble.py
+++ chartjsbubble.py
@@ -81,7 +81,7 @@ def chartjs_plot(filtered_df, markersize, hover_data, color, x_axis, y_axis, yea
             "borderColor": group_info["color"],
             "_originalBackgroundColor": group_info["color"], # <-- STORE ORIGINAL COLOR
             "borderWidth": 0,
-            "hoverRadius": 2
+            "hoverRadius": 5
         }
         for category, group_info in grouped_data.items()
     ]
