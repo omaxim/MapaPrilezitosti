3627db88ff0bcf78514227a862a1fc3a87e23380
omaxim <maxim.oweyssi@gmail.com>
2025-04-10 16:30:41 +0200

    Update chartjsbubble.py

--- chartjsbubble.py
+++ chartjsbubble.py
@@ -157,7 +157,7 @@ def chartjs_plot(filtered_df,markersize,hover_data,color,x_axis,y_axis,year):
                 transitions: {{
                     active: {{
                         animation: {{
-                            duration: 300,
+                            duration: 100,
                             easing: 'easeInQuad'
                         }}
                     }}
