742c09bab8c16554c8aaee4b793ee06a3265edf4
omaxim <maxim.oweyssi@gmail.com>
2025-04-11 12:21:46 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -177,10 +177,10 @@ if st.sidebar.button('Obnovit Data'):
 #hover_data = {col: True for col in hover_info}
 hover_data = get_hover_data(year,year_placeholder,hover_info,x_axis,y_axis,markersize)
 
-if HS_select == []:
-    chart_js = chartjs_plot(filtered_df,markersize,hover_data,color,x_axis,y_axis,year)
 if Skupina is None:
     chart_js = None
+else if HS_select == []:
+    chart_js = chartjs_plot(filtered_df,markersize,hover_data,color,x_axis,y_axis,year)
 else:
     chart_js = chartjs_plot(filtered_df[filtered_df['HS_Lookup'].isin(HS_select)],markersize,hover_data,color,x_axis,y_axis,year)
 
