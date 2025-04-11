2f0df80f2dab9b8dfb3e29eb264fa3e743223a77
omaxim <maxim.oweyssi@gmail.com>
2025-04-11 10:29:19 +0200

    Update visualsetup.py

--- visualsetup.py
+++ visualsetup.py
@@ -54,6 +54,7 @@ def load_visual_identity(header_image_path, background_image_path = 'background.
                 background-repeat: no-repeat;
                 background-size: cover;
                 background-position: center;
+                z-index: 1;
                 opacity: 0.8; /* adjust to taste */
             }}
 
