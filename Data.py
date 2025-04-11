09caeaa7090a2ee6c958a1d56ecee20b7c355474
omaxim <maxim.oweyssi@gmail.com>
2025-04-11 12:21:19 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -179,6 +179,8 @@ hover_data = get_hover_data(year,year_placeholder,hover_info,x_axis,y_axis,marke
 
 if HS_select == []:
     chart_js = chartjs_plot(filtered_df,markersize,hover_data,color,x_axis,y_axis,year)
+if Skupina is None:
+    chart_js = None
 else:
     chart_js = chartjs_plot(filtered_df[filtered_df['HS_Lookup'].isin(HS_select)],markersize,hover_data,color,x_axis,y_axis,year)
 
