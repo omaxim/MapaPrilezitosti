99701766ed3f2fd635869fcf1c79c358588a21f4
omaxim <maxim.oweyssi@gmail.com>
2025-04-11 11:38:49 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -21,9 +21,8 @@ logocol1,logocol2 = st.columns([2,3])
 logocol1.image('logo_web.svg',use_container_width=True)
 
 col1,col2, = st.columns([10,5])
-#col1.title("Mapa příležitostí")
-# Sidebar for selecting variables
-
+col1.markdown("***")
+col2.markdown("***")
 col2.subheader("Nastavení grafu")
 
 
