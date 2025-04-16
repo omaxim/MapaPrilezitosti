c7d750ef184f9123f81b4774eab6e22c8b843bf1
omaxim <maxim.oweyssi@gmail.com>
2025-04-16 19:48:28 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -98,7 +98,7 @@ def load_data(datayear):
     df['CZ 2030 Export CZK']        = USD_to_czk*df['CZ 2030 Export CZK'] 
     df['HS_ID']                 = df['HS_ID'].astype(str)
     df['HS_Lookup']              = df['HS_ID']+" - "+df['Název']
-        return df
+    return df
 
 
 # Create lists of display names for the sidebar
