48dbdfcca8ee0e049e94de330988f54db9e6ff33
omaxim <maxim.oweyssi@gmail.com>
2025-04-16 21:20:46 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -288,7 +288,7 @@ if not (st.session_state.filtrovat_dle_skupin and Skupina is None):
     )
 
 # Example: render the polar area chart in a Streamlit component
-polar_js = chartjs_polar_area(filtered_df_2022, filtered_df_2023, group_field="Kategorie",
+polar_js = chartjs_polar_area(filtered_df_2022, filtered_df_2023, group_field="Skupina",
                               chart_title="Růst exportu podle kategorie",
                               bottom_text="Růst vyjadřuje změnu mezi lety 2022 a 2023")
 st.components.v1.html(polar_js, height=750)
