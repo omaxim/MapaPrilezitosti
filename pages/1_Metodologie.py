a12f4b393c57edbc3a129887994d1c592315c9b2
omaxim <maxim.oweyssi@gmail.com>
2025-04-23 13:23:07 +0200

    Update 1_Metodologie.py

--- pages/1_Metodologie.py
+++ pages/1_Metodologie.py
@@ -71,32 +71,32 @@ Některé kategorie zatím nejsou obsazené, ale počítáme s jejich doplnění
 """)
 col2.markdown("""
 Snížení celkové emisní náročnosti/
-    Snížení emisí výroby/
-        ocel, cement, efektivita, elektrifikace průmyslu i zemědělství
-    Snížení emisí dopravy (rozvoj vlaků)/
-        elektromobilita, vodík, infrastruktura
-    Snížení emisí budov/
-        izolace, elektrifikace vytápění
-    Snížení emisí energie/
-        nízkoemisní elektřina a paliva (vítr, FVE)
-    Ukládání energie/
-    Posílení sítí/
-        elektrické a distribuční sítě, elektrifikace
-    Zadržování uhlíku v krajině/
-        půda a lesnictví
-    Zachytávání a ukládání CO₂/
+├── Snížení emisí výroby/
+│   ├── ocel, cement, efektivita, elektrifikace průmyslu i zemědělství
+├── Snížení emisí dopravy (rozvoj vlaků)/
+│   ├── elektromobilita, vodík, infrastruktura
+├── Snížení emisí budov/
+│   ├── izolace, elektrifikace vytápění
+├── Snížení emisí energie/
+│   ├── nízkoemisní elektřina a paliva (vítr, FVE)
+├── Ukládání energie/
+├── Posílení sítí/
+│   ├── elektrické a distribuční sítě, elektrifikace
+├── Zadržování uhlíku v krajině/
+│   ├── půda a lesnictví
+├── Zachytávání a ukládání CO₂/
 Snížení materiálové náročnosti/
-    redesign produktů a balení/
-    sběr, třídění, přepoužití, recyklace/
+├── redesign produktů a balení/
+├── sběr, třídění, přepoužití, recyklace/
 Ochrana životního prostředí/
-    distribuce vody, snížení znečištění, ochrana biodiverzity/
+├── distribuce vody, snížení znečištění, ochrana biodiverzity/
 Příprava na nepříznivé klima/
-    živelné pohromy, sucho, nové zdroje bílkovin/
+├── živelné pohromy, sucho, nové zdroje bílkovin/
 Měřící a diagnostické přístroje/
-    termostaty, senzory, spektrometry, chemická analýza/
+├── termostaty, senzory, spektrometry, chemická analýza/
 Materiály a komponenty/
-    vzácné kovy, alternativy chemických látek/
-    alternativní pohony a stroje/
+├── vzácné kovy, alternativy chemických látek/
+├── alternativní pohony a stroje/
 """)
 # --- Key Indicators ---
 col2.markdown("### Na jaké ukazatele se zaměřujeme")
