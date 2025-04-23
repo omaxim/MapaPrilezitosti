7da4e3fbb112945ea037a0d8af9f37138b2f3672
omaxim <maxim.oweyssi@gmail.com>
2025-04-23 11:27:14 +0200

    Update highchartpolararea.py

--- highchartpolararea.py
+++ highchartpolararea.py
@@ -109,8 +109,8 @@ def chart_highcharts_variable_pie(filtered_df_2022, filtered_df_2023,
             "headerFormat": "",
             "pointFormat": (
                 '<span style="color:{point.color}">\u25CF</span> <b>{point.name}</b><br/>' +
-                '2022: {point.export22:,.1f} miliard USD<br/>' +
-                '2023: {point.export23:,.1f} miliard USD<br/>' +
+                'Export 2022: {point.export22:,.1f} miliard USD<br/>' +
+                'Export 2023: {point.export23:,.1f} miliard USD<br/>' +
                 'Růst: {point.growth_abs:,.1f} miliard USD ({point.growth_frac:.2f}%)'
             )
         },
