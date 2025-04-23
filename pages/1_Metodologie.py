7f942bc1d3f4502dcfb633573307f5ff479d5759
omaxim <maxim.oweyssi@gmail.com>
2025-04-23 13:24:32 +0200

    Update 1_Metodologie.py

--- pages/1_Metodologie.py
+++ pages/1_Metodologie.py
@@ -70,33 +70,33 @@ Zelené výrobky třídíme do **6 Taxonomických grup**, **17 Skupin**, **40 Po
 Některé kategorie zatím nejsou obsazené, ale počítáme s jejich doplněním.
 """)
 col2.markdown("""
-Snížení celkové emisní náročnosti/
-├── Snížení emisí výroby/
-│   ├── ocel, cement, efektivita, elektrifikace průmyslu i zemědělství
-├── Snížení emisí dopravy (rozvoj vlaků)/
+Snížení celkové emisní náročnosti\n
+├── Snížení emisí výroby\n
+│   ├── ocel, cement, efektivita, elektrifikace průmyslu i zemědělství\n
+├── Snížení emisí dopravy (rozvoj vlaků)\n
 │   ├── elektromobilita, vodík, infrastruktura
-├── Snížení emisí budov/
+├── Snížení emisí budov\n
 │   ├── izolace, elektrifikace vytápění
-├── Snížení emisí energie/
+├── Snížení emisí energie\n
 │   ├── nízkoemisní elektřina a paliva (vítr, FVE)
-├── Ukládání energie/
-├── Posílení sítí/
+├── Ukládání energie\n
+├── Posílení sítí\n
 │   ├── elektrické a distribuční sítě, elektrifikace
-├── Zadržování uhlíku v krajině/
+├── Zadržování uhlíku v krajině\n
 │   ├── půda a lesnictví
-├── Zachytávání a ukládání CO₂/
-Snížení materiálové náročnosti/
-├── redesign produktů a balení/
-├── sběr, třídění, přepoužití, recyklace/
-Ochrana životního prostředí/
-├── distribuce vody, snížení znečištění, ochrana biodiverzity/
-Příprava na nepříznivé klima/
-├── živelné pohromy, sucho, nové zdroje bílkovin/
-Měřící a diagnostické přístroje/
-├── termostaty, senzory, spektrometry, chemická analýza/
-Materiály a komponenty/
-├── vzácné kovy, alternativy chemických látek/
-├── alternativní pohony a stroje/
+├── Zachytávání a ukládání CO₂\n
+Snížení materiálové náročnosti\n
+├── redesign produktů a balení\n
+├── sběr, třídění, přepoužití, recyklace\n
+Ochrana životního prostředí\n
+├── distribuce vody, snížení znečištění, ochrana biodiverzity\n
+Příprava na nepříznivé klima\n
+├── živelné pohromy, sucho, nové zdroje bílkovin\n
+Měřící a diagnostické přístroje\n
+├── termostaty, senzory, spektrometry, chemická analýza\n
+Materiály a komponenty\n
+├── vzácné kovy, alternativy chemických látek\n
+├── alternativní pohony a stroje\n
 """)
 # --- Key Indicators ---
 col2.markdown("### Na jaké ukazatele se zaměřujeme")
