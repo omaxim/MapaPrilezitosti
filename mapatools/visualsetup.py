e5d879572c8c8a12d0c58d60ec65b87788aab970
omaxim <maxim.oweyssi@gmail.com>
2025-04-24 16:43:46 +0200

    Update visualsetup.py

--- mapatools/visualsetup.py
+++ mapatools/visualsetup.py
@@ -197,12 +197,10 @@ def load_visual_identity(header_image_path, background_image_path = 'resources/b
         /* Main container using flexbox */
         .logo-header-container {{
             display: flex;
-            justify-content: left; 
-
             align-items: center; /* Vertically center items */
             gap: 200px; /* Space between text logo and partner logos */
             margin-bottom: 20px; /* Add some space below the logo section */
-            flex-wrap: wrap; /* Allow wrapping if needed */
+            flex-wrap: nowrap; /* Allow wrapping if needed */
         }}
 
         /* Style for the text logo */
