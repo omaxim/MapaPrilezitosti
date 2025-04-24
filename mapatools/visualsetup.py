b1bd7f236cff8e094a3eda0139c2259e2e0e52b9
omaxim <maxim.oweyssi@gmail.com>
2025-04-24 16:12:16 +0200

    Update visualsetup.py

--- mapatools/visualsetup.py
+++ mapatools/visualsetup.py
@@ -88,7 +88,7 @@ def load_visual_identity(header_image_path, background_image_path = 'resources/b
                 bottom: 0;
                 width: 100%;
                 height: 100%;  /* Match header-image height */
-                background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 55%, rgba(255, 255, 255, 1) 100%);
+                background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 60%, rgba(255, 255, 255, 1) 100%);
                 z-index: 1;
             }}
             
