d659ca85c8d01cf6dd3aae0f4b047a45aa86a0a1
omaxim <maxim.oweyssi@gmail.com>
2025-04-25 10:45:55 +0200

    Update visualsetup.py

--- mapatools/visualsetup.py
+++ mapatools/visualsetup.py
@@ -90,7 +90,7 @@ def load_visual_identity(header_image_path, background_image_path = 'resources/b
                     height: 70vh;  /* Smaller height on phones */
                 }}
                 .header-gradient {{
-                    height: 100%;  /* Match header-image height */
+                    height: 60%;  /* Match header-image height */
                     background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 65%, rgba(255, 255, 255, 1) 100%);
 
                     }}
@@ -187,7 +187,6 @@ def load_visual_identity(header_image_path, background_image_path = 'resources/b
         }}
         @media (max-width: 768px) {{
             .logo-container {{
-                padding-top: -60vh; 
                 justify-content: center;
             }}
         }}
