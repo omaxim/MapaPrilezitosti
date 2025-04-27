8dc56e455495831f3e8eee64ca1f853db4c46811
omaxim <maxim.oweyssi@gmail.com>
2025-04-27 23:34:59 +0200

    Update 1_Metodologie.py

--- pages/1_Metodologie.py
+++ pages/1_Metodologie.py
@@ -157,8 +157,10 @@ hhicol1,hhicol2 = col2.columns(2)
 hhicol1.latex(r"\text{HHI} = \sum_{i=1}^N s_i^2")
 hhicol2.markdown(
 """
- kde $s_i$ je tržní podíl země $i$ v procentech.
- """,
+ 
+ 
+kde $s_i$ je tržní podíl země $i$ v procentech.
+""",
 unsafe_allow_html=True,)
 col2.markdown(
 """
