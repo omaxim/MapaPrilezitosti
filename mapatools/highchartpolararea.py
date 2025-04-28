385e3b7aa1376a762223661a77ce2c08d4ca96d7
omaxim <maxim.oweyssi@gmail.com>
2025-04-28 11:49:05 +0200

    Update highchartpolararea.py

--- mapatools/highchartpolararea.py
+++ mapatools/highchartpolararea.py
@@ -89,7 +89,7 @@ def chart_highcharts_variable_pie(filtered_df_2022, filtered_df_2023,
             "growth_abs": (export_23 - export_22) / 1e9,
             "growth_frac": 100 * growth
         })
-    sorted_data_series = sorted(data_series, key=lambda item: item['z'], reverse=False)
+    sorted_data_series = sorted(data_series, key=lambda item: item['z'], reverse=True)
 
     # Highcharts config
     chart_config = {
